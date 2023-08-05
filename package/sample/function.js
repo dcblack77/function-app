b584f24b54c0492698dbf1c64a79d2fdd1c8f884
Diego D. Cabrera R <diegocabrera123@gmail.com>
2023-08-05 16:58:51 +0200

    first commit

---
+++ package/sample/function.js
@@ -0,0 +1,8 @@
+async function main(args) {
+  let name = args.name || 'stranger'
+  let greeting = 'Hello ' + name + '!'
+  console.log(greeting)
+  return {"body": greeting}
+}
+
+exports.main = main;
