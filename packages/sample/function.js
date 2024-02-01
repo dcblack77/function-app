b2d60fbec7d7af734c851ebaa53a4ae6f4361b1f
Diego D Cabrera Rojas <diegocabrera123@gmail.com>
2024-02-01 18:11:03 +0100

    feat: new message

--- packages/sample/function.js
+++ packages/sample/function.js
@@ -3,7 +3,7 @@ async function main(args) {
   const name = args.name || 'stranger'
   const greeting = 'Hello ' + name + '!'
   console.log(greeting)
-  return {"body": greeting}
+  return { "body": greeting }
 }
 
 exports.main = main;
